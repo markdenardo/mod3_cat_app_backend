fedf00886df9d9c5f417915c9993e1debbea8fc7
markdenardo <mark.denardo@gmail.com>
2020-04-24 17:19:30 -0400

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bad-cat-app.herokuapp.com/'
+    origins 'https://markdenardo.github.io'
 
-    resource 'https://bad-cat-app.herokuapp.com/',
+    resource 'https://markdenardo.github.io',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
