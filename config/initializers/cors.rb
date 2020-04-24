0db7a4a115e01adc4e914bb8315d877742b594e8
markdenardo <mark.denardo@gmail.com>
2020-04-24 17:26:05 -0400

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://markdenardo.github.io'
-
-    resource 'https://markdenardo.github.io',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource 'https://markdenardo.github.io'
+    Access-Control-Allow-Headers: origin, content-type
+    Access-Control-Allow-Methods: GET, POST, PUT, DELETE
+      # headers: :any,
+      # methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
