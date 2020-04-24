8541111bbf564d263dbcc2ec4ba0bd24e38a3030
markdenardo <mark.denardo@gmail.com>
2020-04-24 17:48:19 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://markdenardo.github.io/mod3_cat_app_frontend/'
+    origins '*'
 
     resource '*',
       headers: :any,
